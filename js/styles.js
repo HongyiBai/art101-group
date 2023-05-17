21051623e42d8ac1a262617210495c9b2c470b93
Abovee1 <84465361+Abovee1@users.noreply.github.com>
2023-05-17 14:27:16 -0700

    Update styles.js

---
+++ js/styles.js
@@ -0,0 +1,29 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+function jump(){
+    if(dino.classList != "jump"){
+        dino.classList.add("jump");
+
+    setTimeout(function (){
+        dino.classList.remove("jump");
+        }, 300);
+    }
+}
+let isAlive = setInterval(function () {
+    //get current dino Y position
+    let dinoTop = parseInt(
+    window.getComputedStyle(dino).getPropertyValue("top"));
+
+    //Get current cactus x position
+    let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left"));
+    //detect collision
+    if (cactusLeft <50 && cactusLeft> 0 && dinoTop>=140) {
+        //collision
+        alert("Game Over!");
+        }
+}, 10);
+
+document.addEventListener("keydown", function (event){
+    jump();
+});
