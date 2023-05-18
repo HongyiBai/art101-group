c146c322b2f69a383f911281f4011ad32584a3dc
Abovee1 <84465361+Abovee1@users.noreply.github.com>
2023-05-17 17:30:42 -0700

    Update styles.js

--- js/styles.js
+++ js/styles.js
@@ -1,29 +1,29 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
-function jump(){
-    if(dino.classList != "jump"){
+function jump() {
+    if (dino.classList != "jump") {
         dino.classList.add("jump");
 
-    setTimeout(function (){
-        dino.classList.remove("jump");
+        setTimeout(function () {
+            dino.classList.remove("jump");
         }, 300);
     }
 }
 let isAlive = setInterval(function () {
     //get current dino Y position
     let dinoTop = parseInt(
-    window.getComputedStyle(dino).getPropertyValue("top"));
+        window.getComputedStyle(dino).getPropertyValue("top"));
 
     //Get current cactus x position
     let cactusLeft = parseInt(
-    window.getComputedStyle(cactus).getPropertyValue("left"));
+        window.getComputedStyle(cactus).getPropertyValue("left"));
     //detect collision
-    if (cactusLeft <50 && cactusLeft> 0 && dinoTop>=140) {
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
         //collision
         alert("Game Over!");
-        }
+    }
 }, 10);
 
-document.addEventListener("keydown", function (event){
+document.addEventListener("keydown", function (event) {
     jump();
 });
